f81ab5cc22f2b3545ed835f74bfcfb2423035c61
SamuelSwanson <77930358+SamuelSwanson@users.noreply.github.com>
2024-08-15 23:09:23 -0400

    Adding Router Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './informaswan-pages/common/landing-page/landing-page.component';
+import { WorkoutExercisesComponent} from './informaswan-pages/athletics/workout-exercises/workout-exercises.component';
 
 export const routes: Routes = [
-    { path: '',     component: LandingPageComponent}
+    { path: '', redirectTo:'/landing-page', pathMatch: 'full'},
+    { path:'landing-page' ,  component: LandingPageComponent}, 
+    { path:'workout-exercises', component: WorkoutExercisesComponent}
 ];

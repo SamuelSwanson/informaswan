e5b7275fbec830d0409b447167ec7f388bfa3f2a
SamuelSwanson <77930358+SamuelSwanson@users.noreply.github.com>
2024-09-09 21:29:23 -0400

    Auto-generated commit

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-KW42E3ZM.mjs";import"./chunk-IWC6FJUK.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LandingPageComponent } from './informaswan-pages/common/landing-page/landing-page.component';
-import { WorkoutExercisesComponent} from './informaswan-pages/athletics/workout-exercises/workout-exercises.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo:'/home', pathMatch: 'full'},
-    { path:'home' ,  component: LandingPageComponent}, 
-    { path:'workout-exercises', component: WorkoutExercisesComponent}
-];
